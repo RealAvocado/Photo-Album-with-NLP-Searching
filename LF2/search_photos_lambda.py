846702194a1bd050d2e1b4d70b326e1eee94252d
RealAvocado <Letian.Jiang1106@outlook.com>
2023-12-02 00:03:52 -0500

    Lambda Function 2 finished. Need to be tested.

--- LF2/search_photos_lambda.py
+++ LF2/search_photos_lambda.py
@@ -1,64 +1,63 @@
+import json
+import boto3
+import requests
+
+from requests.auth import HTTPBasicAuth
+
 def lambda_handler(event, context):
-    response = prepare_lex_response(event)
-    return response
-
-
-def prepare_lex_response(event):
+    session_id = event[sessionId]
     intent = event['sessionState']['intent']['name']
-    session_id = event['sessionId']
-    request_id = event['sessionState']['originatingRequestId']
-    nluConfidence = event['interpretations']['nluConfidence']
+    first_photo_search_intent = event['sessionState']['intent']['slots'].get(["FirstPhotoIntent"], 'null')
+    second_photo_search_intent = event['sessionState']['intent']['slots'].get(["SecondPhotoIntent"], 'null')
+
+    send_response_to_lex(session_id, intent, first_photo_search_intent, second_photo_search_intent)
+
+    search_intents = []
+    # if no intent is fulfilled, return empty array
+    if intent == 'FallbackIntent':
+        return search_intents
+
+    # else, at least one intent is fulfilled
+    search_intents.append(first_photo_search_intent)
+    if second_photo_search_intent != 'null':
+        search_intents.append(second_photo_search_intent)
+
+    photo_URLs = search_OpenSearch_index(search_intents)
+    return {
+        'statusCode': 200,
+        'headers': {"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Methods": "*",
+                    "Access-Control-Allow-Headers": "*"},
+        'body': json.dumps(photo_URLs)
+    }
+
+
+def send_response_to_lex(session_id, intent, first_photo_search_intent, second_photo_search_intent):
+    client = boto3.client('lexv2-runtime')
 
     # no intent is recgonized
     if intent == 'FallbackIntent':
-        response = {
+        lex_response = {
+            "botId": "8JKTFL3V5D",
+            "botAliasId": "TSTALIASID",
+            "localeId": "en_US",
+            "sessionId": session_id,
             "sessionState": {
                 "dialogAction": {
                     "type": "Close"
-                },
-                "intent": {
-                    "name": "FallbackIntent",
-                    "slots": {},
-                    "state": "ReadyForFulfillment",
-                    "confirmationState": "None"
-                },
-                "sessionAttributes": {},
-                "originatingRequestId": request_id
-            },
-            "interpretations": [
-                {
-                    "intent": {
-                        "name": "FallbackIntent",
-                        "slots": {},
-                        "state": "ReadyForFulfillment",
-                        "confirmationState": "None"
-                    },
-                    "interpretationSource": "Lex"
-                },
-                {
-                    "nluConfidence": {
-                        "score": nluConfidence
-                    },
-                    "intent": {
-                        "name": "PhotoSearchIntent",
-                        "slots": {
-                            "FirstPhotoIntent": null,
-                            "SecondPhototIntent": null
-                        }
-                    },
-                    "interpretationSource": "Lex"
                 }
-            ],
-            "sessionId": session_id
+            }
         }
+        response = client.put_session(lex_response)
         return response
 
-    first_photo_search_intent = event['sessionState']['intent']['slots']["FirstPhotoIntent"]
-    second_photo_search_intent = event['sessionState']['intent']['slots']["SecondPhotoIntent"]
-
+    # only the first slot is fullfilled
     if first_photo_search_intent != 'null' and second_photo_search_intent == 'null':
         first_slot_value = first_photo_search_intent['value']['originalValue']
-        response = {
+        lex_response = {
+            "botId": "8JKTFL3V5D",
+            "botAliasId": "TSTALIASID",
+            "localeId": "en_US",
+            "sessionId": session_id,
             "messages": [
                 {
                     "content": "Enter the photo types.",
@@ -69,137 +68,46 @@ def prepare_lex_response(event):
                 "dialogAction": {
                     "type": "ElicitSlot",
                     "slotToElicit": "SecondPhototIntent"
-                },
-                "intent": {
-                    "name": "PhotoSearchIntent",
-                    "slots": {
-                        "FirstPhotoIntent": {
-                            "value": {
-                                "originalValue": first_slot_value,
-                                "interpretedValue": first_slot_value,
-                                "resolvedValues": [
-                                    first_slot_value
-                                ]
-                            }
-                        },
-                        "SecondPhototIntent": null
-                    },
-                    "state": "InProgress",
-                    "confirmationState": "None"
-                },
-                "sessionAttributes": {},
-                "originatingRequestId": request_id
-            },
-            "interpretations": [
-                {
-                    "nluConfidence": {
-                        "score": nluConfidence
-                    },
-                    "intent": {
-                        "name": "PhotoSearchIntent",
-                        "slots": {
-                            "FirstPhotoIntent": {
-                                "value": {
-                                    "originalValue": first_slot_value,
-                                    "interpretedValue": first_slot_value,
-                                    "resolvedValues": [
-                                        first_slot_value
-                                    ]
-                                }
-                            },
-                            "SecondPhototIntent": null
-                        },
-                        "state": "InProgress",
-                        "confirmationState": "None"
-                    },
-                    "interpretationSource": "Lex"
-                },
-                {
-                    "intent": {
-                        "name": "FallbackIntent",
-                        "slots": {}
-                    },
-                    "interpretationSource": "Lex"
                 }
-            ],
-            "sessionId": session_id
+            }
         }
+        response = client.put_session(lex_response)
+        return response
 
+    # both the first and second slot are fullfilled
     elif first_photo_search_intent != 'null' and second_photo_search_intent != 'null':
         first_slot_value = first_photo_search_intent['value']['originalValue']
         second_slot_value = secondt_photo_search_intent['value']['originalValue']
-        response = {
+        lex_response = {
+            "botId": "8JKTFL3V5D",
+            "botAliasId": "TSTALIASID",
+            "localeId": "en_US",
+            "sessionId": session_id,
             "sessionState": {
                 "dialogAction": {
                     "type": "Close"
-                },
-                "intent": {
-                    "name": "PhotoSearchIntent",
-                    "slots": {
-                        "FirstPhotoIntent": {
-                            "value": {
-                                "originalValue": first_slot_value,
-                                "interpretedValue": first_slot_value,
-                                "resolvedValues": [
-                                    first_slot_value
-                                ]
-                            }
-                        },
-                        "SecondPhototIntent": {
-                            "value": {
-                                "originalValue": second_slot_value,
-                                "interpretedValue": second_slot_value,
-                                "resolvedValues": [
-                                    second_slot_value
-                                ]
-                            }
-                        }
-                    },
-                    "state": "ReadyForFulfillment",
-                    "confirmationState": "None"
-                },
-                "sessionAttributes": {},
-                "originatingRequestId": request_id
-            },
-            "interpretations": [
-                {
-                    "nluConfidence": {
-                        "score": nluConfidence
-                    },
-                    "intent": {
-                        "name": "PhotoSearchIntent",
-                        "slots": {
-                            "FirstPhotoIntent": {
-                                "value": {
-                                    "originalValue": first_slot_value,
-                                    "interpretedValue": first_slot_value,
-                                    "resolvedValues": [
-                                        first_slot_value
-                                    ]
-                                }
-                            },
-                            "SecondPhototIntent": {
-                                "value": {
-                                    "originalValue": second_slot_value,
-                                    "interpretedValue": second_slot_value,
-                                    "resolvedValues": [
-                                        second_slot_value
-                                    ]
-                                }
-                            }
-                        },
-                        "state": "ReadyForFulfillment",
-                        "confirmationState": "None"
-                    },
-                    "interpretationSource": "Lex"
-                },
-                {
-                    "intent": {
-                        "name": "FallbackIntent",
-                        "slots": {}
-                    },
-                    "interpretationSource": "Lex"
                 }
-            ],
-            "sessionId": session_id
+            }
         }
+        response = client.put_session(lex_response)
+        return response
+
+
+def search_OpenSearch_index(search_intents):
+    photo_URLs = []
+
+    username = 'opensearch-user'
+    password = 'h7-VUV21[o87hbtyuR'
+
+    host = 'https://search-photos-a2wikxciocdgnhqczvcuy6zzqa.us-east-1.es.amazonaws.com'
+    index = 'photos'
+    search_url_base = host + '/' + index + '/' + '_search?q='
+
+    for intent in search_intents:
+        search_url = search_url_base + intent
+        response = requests.get(search_url, auth=HTTPBasicAuth(username, password))
+        photo = response['hits']['hits']['_source']['objectKey']
+        photo_url = 'https://photo-bucket-2397.s3.amazonaws.com/' + photo
+        photo_URLs.append(photo_url)
+
+    return photo_URLs
