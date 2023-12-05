0e0f997d7a3b8f6cc1745365159e1f5ecfbd26e5
RealAvocado <Letian.Jiang1106@outlook.com>
2023-12-05 17:03:36 -0500

    Custom labels supported.

--- LF1/index_photos_lambda.py
+++ LF1/index_photos_lambda.py
@@ -5,6 +5,7 @@ import requests
 from datetime import datetime
 from requests.auth import HTTPBasicAuth
 
+
 def lambda_handler(event, context):
     s3 = boto3.client('s3', region_name='us-east-1')
     rekognition = boto3.client('rekognition', region_name='us-east-1')
@@ -34,10 +35,12 @@ def lambda_handler(event, context):
 
         # Step ii: Retrieve metadata
         metadata = s3.head_object(Bucket=bucket, Key=key)
-        custom_labels = json.loads(metadata.get('x-amz-meta-customLabels', '[]'))
+        sys.stdout.write(str(metadata))
+        custom_labels = metadata['Metadata'].get('customlabels', None)
 
         # Combine labels from Rekognition and custom labels
-        labels.extend(custom_labels)
+        if custom_labels is not None:
+            labels.append(custom_labels.split(','))
 
         # Step iii: Store JSON object in OpenSearch
         opensearch_payload = {
@@ -52,6 +55,7 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps('Indexing complete!')
     }
+
 
 def insert_into_opensearch(json_data, opensearch_endpoint, http_auth):
     headers = {'Content-Type': 'application/json'}
@@ -69,6 +73,7 @@ def insert_into_opensearch(json_data, opensearch_endpoint, http_auth):
 
     return response
 
+
 def delete_old_photo_with_same_name(opensearch_endpoint, username, password, index, photo_name):
     search_url = opensearch_endpoint + '/' + index + '/' + '_search?q=' + photo_name
     response = requests.get(search_url, auth=HTTPBasicAuth(username, password))
@@ -79,3 +84,30 @@ def delete_old_photo_with_same_name(opensearch_endpoint, username, password, ind
         document_id = response['hits']['hits'][0]['_id']
         delete_url = opensearch_endpoint + '/' + index + '/' + '_doc' + '/' + document_id
         delete_response = requests.delete(delete_url, auth=HTTPBasicAuth(username, password))
+
+
+{
+    'ResponseMetadata':
+     {
+        'RequestId': 'AQCCDS58SV29GRP9',
+        'HostId': 'Ao23PzICP0iu01BvK+K3Lro0F86P95DGCp8iz5yLqVSJdbyTvt0d9Vw6Ia2PUsr1hjSxyN7La+4=',
+        'HTTPStatusCode': 200, 'HTTPHeaders': {
+        'x-amz-id-2': 'Ao23PzICP0iu01BvK+K3Lro0F86P95DGCp8iz5yLqVSJdbyTvt0d9Vw6Ia2PUsr1hjSxyN7La+4=',
+        'x-amz-request-id': 'AQCCDS58SV29GRP9', 'date': 'Tue, 05 Dec 2023 19:36:17 GMT',
+        'last-modified': 'Tue, 05 Dec 2023 19:36:09 GMT', 'etag': '"368ea2e110e2776ece6a019c63dd28d0"',
+        'x-amz-server-side-encryption': 'AES256', 'x-amz-meta-customlabels': 'cute&fat', 'accept-ranges': 'bytes',
+        'content-type': 'image/jpeg', 'server': 'AmazonS3', 'content-length': '51937'
+     },
+      'RetryAttempts': 0
+      },
+    'AcceptRanges': 'bytes',
+    'LastModified': datetime.datetime(2023, 12, 5, 19, 36, 9, tzinfo=tzutc()),
+    'ContentLength': 51937,
+    'ETag': '"368ea2e110e2776ece6a019c63dd28d0"',
+    'ContentType': 'image/jpeg',
+    'ServerSideEncryption': 'AES256',
+    'Metadata':
+        {
+            'customlabels': 'cute&fat'
+        }
+ }
